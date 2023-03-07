4ee11e3c09eaef65a5057dd1b38963c1f71a296e
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-07 15:46:05 +0600

    first commit

---
+++ js/append.js
@@ -0,0 +1,7 @@
+const placesList = document.getElementById("places-list");
+const li = document.createElement("li");
+li.innerText = "Pahertoli bon";
+placesList.appendChild(li);
+// 
+const mainContainer = document.getElementById("main-container");
+console.log(mainContainer);
